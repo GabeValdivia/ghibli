8c06c3a1a59b712b603a9d19df69c5b3b5ee3216
Gabe Valdivia <gabe@gabeswebcoding.com>
2021-06-05 23:21:13 -0500

    Add logo image and appendChild logo and a new div with a class of 'container'. Pull in description from API.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,21 @@
+const app = document.getElementById('root')
+
+const logo = document.createElement('img')
+logo.src = '/images/logo.png'
+
+const container  = document.createElement('div')
+container.setAttribute('class','container')
+
+app.appendChild(logo)
+app.appendChild(container)
+
 //Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest()
 
 //Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 
-request.onload = function() {
+request.onload = function () {
     //Begin accessing JSON data here
     var data = JSON.parse(this.response)
 
@@ -12,6 +23,7 @@ request.onload = function() {
         data.forEach((movie) => {
             //Log each movie's title
             console.log(movie.title)
+            console.log(movie.description)
         })
     } else {
         console.log('error')
@@ -20,4 +32,3 @@ request.onload = function() {
 
 //Send request
 request.send()
-
